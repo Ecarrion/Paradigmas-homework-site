502882a33f6b2955576352381084808b24a21f48
Ernesto Carrión <carrion.silver@gmail.com>
2011-02-15 21:47:24 -0500

    Added some styling, footers and headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  #Assembles a custom title for every page
+  def title
+    base = "Paradigms"
+    if @title
+      base += " | #{@title}"
+    end
+  end
+  
+  #Places a logo image and a link to the root page
+  def logo(path, params)
+   l = image_tag(path, params)     
+   link_to l, path
+  end
+  
 end
