da82aeb8bcbda5624116ad15e6d067b75a539f01
Ernesto Carrión <carrion.silver@gmail.com>
2011-02-18 03:50:48 -0500

    Configuring some env variables

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -29,7 +29,9 @@ class Homework < ActiveRecord::Base
   #paperclip
   has_attached_file :file,
       :storage => :s3,
-      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+      :s3_credentials => {:access_key_id => ENV['S3_KEY'],
+                          :secret_access_key => ENV['S3_SECRET'],
+                          :bucket => ENV['S3_BUCKET']},
       :path => ":attachment/:#{:id}-:#{:filename}"
   
 end
